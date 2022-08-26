1e7f242a86dbf1bf8129229dac2b1796183de451
claire <1097457668@qq.com>
2022-08-26 16:04:03 +0200

    corriger les fault

--- show_all_lists.py
+++ show_all_lists.py
@@ -3,31 +3,35 @@
 import pymongo
 from pymongo import MongoClient
 
+
 def get_collection():
-    #client = MongoClient()
-    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-    db = myclient.list_database_names()
+    # client = MongoClient()
+    my_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = my_client.list_database_names()
     if "runoobdb" in db:
-        print("database deja exist !")
+        print("database already exist !")
   
     # collection = db.python_collection
     hives = db.hives
     return hives
 
-def check_exist(coll,data):
+
+def check_exist(coll, data):
     query_filter = {'name': data['name']}
     cnt = coll.find(query_filter).count()
     return cnt > 0
 
+
+# 打印所有蜂巢信息
 def page_query(query_filter=None, page_size=200, page_on=1):
     skip = page_size * (page_on - 1)
     coll = get_collection()
     page_record = coll.find(query_filter).limit(page_size).skip(skip)
     
     for r in page_record:
-        print r
+        print(r)
         
-#if __name__ == "__main__":
+# if __name__ == "__main__":
      
 
 
